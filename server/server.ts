41e950d5789cb3ad9797aa52edb9833031dabe6b
James Anderson <j.anderson2429@gmail.com>
2025-01-13 19:06:24 -0500

    adding concurrently for easier testing and runnning

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-// src/server.ts
+// src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
@@ -15,7 +15,7 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
-app.use("/api", require("../routes/projectRoutes"));
+app.use("/api", require("./routes/projectRoutes"));
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
